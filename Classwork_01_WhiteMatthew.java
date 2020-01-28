e9ca60ab4d84268cb276e634d832a371ae108c0b
whitem1396 <46724878+whitem1396@users.noreply.github.com>
2020-01-28 10:08:53 -0500

    Update Classwork_01_WhiteMatthew.java

--- Classwork_01_WhiteMatthew.java
+++ Classwork_01_WhiteMatthew.java
@@ -1,14 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Creating 3 different displays using the same example and calling them 
+01/28/2020
+Matthew White
  */
+
 package classwork_01_whitematthew;
 
 import java.util.Scanner;  // Needed for the Scanner class
 import javax.swing.JOptionPane;
 /*
-
 */
 
 public class Classwork_01_WhiteMatthew 
@@ -22,10 +22,25 @@ public class Classwork_01_WhiteMatthew
     
         public static void displayPseudocode () {
             
+            System.out.println("I'm creating the display scanner function.");
+            System.out.println("You are going to input your age.");
+            System.out.println("Next you are going to input your annual income.");
+            System.out.println("Then you are going to type just your first name.");
+            System.out.println("The computer isthen going to output all of the "
+                    + "results you entered.");
+            System.out.println("");
+            System.out.println("I'm creating the JOptionPane function.");
+            System.out.println("You are going to type just your first name.");
+            System.out.println("You are going to input your age.");
+            System.out.println("You are going to input your annual income.");
+            System.out.println("The computer is going to output all of the results"
+                    + " you entered.");
+            System.out.println("");
+            
         }
         
         public static void displayScanner () {
-            String name; // To hold the user's name
+        String name; // To hold the user's name
         int age;     // To hold the user's age
         double income; // To hold the user's income
         
@@ -34,10 +49,12 @@ public class Classwork_01_WhiteMatthew
         
         // Get the users age.
         System.out.print("What is your age? ");
+        // Turning age into a integer
         age = keyboard.nextInt();
         
         // Get the user's income.
         System.out.print("What is your annual income? ");
+        // Turning income into a double
         income = keyboard.nextDouble();
         
         // Consume the remaining new line.
@@ -78,7 +95,8 @@ public class Classwork_01_WhiteMatthew
             
             // Display the results.
             JOptionPane.showMessageDialog(null, "Hello " +
-                                           name + "");
+                                           name + " your age is "+ age
+                                           + " your income is $" + income);
             
             
             
@@ -86,5 +104,6 @@ public class Classwork_01_WhiteMatthew
         }
         // TODO code application logic here
     }
+
     
 
